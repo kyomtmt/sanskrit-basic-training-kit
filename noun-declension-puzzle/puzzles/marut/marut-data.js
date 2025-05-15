625a152840df66e06fd2510bd9e53ee9dfdb53ba
kyomtmt <kyowada@gmail.com>
2025-05-15 21:41:45 +0900

    Create marut-data.js
    Create marut-data.js for marut puzzle

---
+++ noun-declension-puzzle/puzzles/marut/marut-data.js
@@ -0,0 +1,13 @@
+// Data for marut declension puzzle
+
+const marutCardsData = [
+    { id: 'card-marut-nom-sg', romaji: 'marut', devanagari: 'मरुत्', correctCell: 'cell-nom-sg' },
+    { id: 'card-marut-acc-sg', romaji: 'marutam', devanagari: 'मरुतम्', correctCell: 'cell-acc-sg' },
+    // ... 他の活用形データをここに追加 ...
+    // 例として数件だけ記述し、後で完全なリストに置き換える
+    { id: 'card-marut-voc-pl', romaji: 'marutaḥ', devanagari: 'मरुतः', correctCell: 'cell-voc-pl' }
+];
+
+// もし、このファイルが読み込まれたことを確認したい場合
+// console.log("marut-data.js loaded");
+// console.log(marutCardsData);
