2eb0f9578fd5b0f712c8ac1be420618847a033e1
kyomtmt <kyowada@gmail.com>
2025-05-18 12:58:00 +0900

    Create tapasvin-data.js
    create tapasvin data

---
+++ noun-declension-puzzle/puzzles/tapasvin/tapasvin-data.js
@@ -0,0 +1,34 @@
+// Data for tapasvin- (m. "ascetic") declension puzzle
+
+const tapasvinCardsData = [
+    // 単数 (Singular)
+    { id: 'card-tapasvin-nom-sg',  romaji: 'tapasvī',      devanagari: 'तपस्वी',     correctCell: 'cell-nom-sg' },
+    { id: 'card-tapasvin-acc-sg',  romaji: 'tapasvinam',   devanagari: 'तपस्विनम्',  correctCell: 'cell-acc-sg' },
+    { id: 'card-tapasvin-ins-sg',  romaji: 'tapasvinā',    devanagari: 'तपस्विना',   correctCell: 'cell-ins-sg' },
+    { id: 'card-tapasvin-dat-sg',  romaji: 'tapasvine',    devanagari: 'तपस्विने',   correctCell: 'cell-dat-sg' },
+    { id: 'card-tapasvin-abl-sg',  romaji: 'tapasvinaḥ',   devanagari: 'तपस्विनः',  correctCell: 'cell-abl-sg' },
+    { id: 'card-tapasvin-gen-sg',  romaji: 'tapasvinaḥ',   devanagari: 'तपस्विनः',  correctCell: 'cell-gen-sg' },
+    { id: 'card-tapasvin-loc-sg',  romaji: 'tapasvini',    devanagari: 'तपस्विनि',   correctCell: 'cell-loc-sg' },
+    { id: 'card-tapasvin-voc-sg',  romaji: 'tapasvin',     devanagari: 'तपस्विन्',   correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    // 表26で空欄の箇所は、一般的なin幹男性名詞のパターンや類似形から補完
+    { id: 'card-tapasvin-nom-du',  romaji: 'tapasvinau',   devanagari: 'तपस्विनौ',  correctCell: 'cell-nom-du' },
+    { id: 'card-tapasvin-acc-du',  romaji: 'tapasvinau',   devanagari: 'तपस्विनौ',  correctCell: 'cell-acc-du' },
+    { id: 'card-tapasvin-ins-du',  romaji: 'tapasvibhyām', devanagari: 'तपस्विभ्याम्',correctCell: 'cell-ins-du' },
+    { id: 'card-tapasvin-dat-du',  romaji: 'tapasvibhyām', devanagari: 'तपस्विभ्याम्',correctCell: 'cell-dat-du' },
+    { id: 'card-tapasvin-abl-du',  romaji: 'tapasvibhyām', devanagari: 'तपस्विभ्याम्',correctCell: 'cell-abl-du' },
+    { id: 'card-tapasvin-gen-du',  romaji: 'tapasvinoḥ',   devanagari: 'तपस्विनोः',  correctCell: 'cell-gen-du' },
+    { id: 'card-tapasvin-loc-du',  romaji: 'tapasvinoḥ',   devanagari: 'तपस्विनोः',  correctCell: 'cell-loc-du' },
+    { id: 'card-tapasvin-voc-du',  romaji: 'tapasvinau',   devanagari: 'तपस्विनौ',  correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural)
+    { id: 'card-tapasvin-nom-pl',  romaji: 'tapasvinaḥ',   devanagari: 'तपस्विनः',  correctCell: 'cell-nom-pl' },
+    { id: 'card-tapasvin-acc-pl',  romaji: 'tapasvinaḥ',   devanagari: 'तपस्विनः',  correctCell: 'cell-acc-pl' },
+    { id: 'card-tapasvin-ins-pl',  romaji: 'tapasvibhiḥ',  devanagari: 'तपस्विभिः', correctCell: 'cell-ins-pl' },
+    { id: 'card-tapasvin-dat-pl',  romaji: 'tapasvibhyaḥ', devanagari: 'तपस्विभ्यः',correctCell: 'cell-dat-pl' },
+    { id: 'card-tapasvin-abl-pl',  romaji: 'tapasvibhyaḥ', devanagari: 'तपस्विभ्यः',correctCell: 'cell-abl-pl' },
+    { id: 'card-tapasvin-gen-pl',  romaji: 'tapasvinām',   devanagari: 'तपस्विनाम्',  correctCell: 'cell-gen-pl' },
+    { id: 'card-tapasvin-loc-pl',  romaji: 'tapasviṣu',    devanagari: 'तपस्विषु',   correctCell: 'cell-loc-pl' },
+    { id: 'card-tapasvin-voc-pl',  romaji: 'tapasvinaḥ',   devanagari: 'तपस्विनः',  correctCell: 'cell-voc-pl' }
+];
