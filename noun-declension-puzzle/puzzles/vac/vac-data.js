e5624f1ca75b9e2f56543e71fa5a79741847515f
kyomtmt <kyowada@gmail.com>
2025-05-18 12:05:33 +0900

    Create vac-data.js
    vacデータ

---
+++ noun-declension-puzzle/puzzles/vac/vac-data.js
@@ -0,0 +1,34 @@
+// Data for vāc- (f. "speech, word") declension puzzle
+
+const vacCardsData = [
+    // 単数 (Singular)
+    { id: 'card-vac-nom-sg',  romaji: 'vāk',      devanagari: 'वाक्',    correctCell: 'cell-nom-sg' },
+    { id: 'card-vac-acc-sg',  romaji: 'vācam',    devanagari: 'वाचम्',   correctCell: 'cell-acc-sg' },
+    { id: 'card-vac-ins-sg',  romaji: 'vācā',     devanagari: 'वाचा',    correctCell: 'cell-ins-sg' },
+    { id: 'card-vac-dat-sg',  romaji: 'vāce',     devanagari: 'वाचे',    correctCell: 'cell-dat-sg' },
+    { id: 'card-vac-abl-sg',  romaji: 'vācaḥ',    devanagari: 'वाचः',   correctCell: 'cell-abl-sg' },
+    { id: 'card-vac-gen-sg',  romaji: 'vācaḥ',    devanagari: 'वाचः',   correctCell: 'cell-gen-sg' },
+    { id: 'card-vac-loc-sg',  romaji: 'vāci',     devanagari: 'वाचि',    correctCell: 'cell-loc-sg' },
+    { id: 'card-vac-voc-sg',  romaji: 'vāk',      devanagari: 'वाक्',    correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    // 表25で空欄の箇所は、一般的なc幹女性名詞のパターンや類似形から補完
+    { id: 'card-vac-nom-du',  romaji: 'vācau',    devanagari: 'वाचौ',   correctCell: 'cell-nom-du' },
+    { id: 'card-vac-acc-du',  romaji: 'vācau',    devanagari: 'वाचौ',   correctCell: 'cell-acc-du' },
+    { id: 'card-vac-ins-du',  romaji: 'vāgbhyām', devanagari: 'वाग्भ्याम्', correctCell: 'cell-ins-du' },
+    { id: 'card-vac-dat-du',  romaji: 'vāgbhyām', devanagari: 'वाग्भ्याम्', correctCell: 'cell-dat-du' },
+    { id: 'card-vac-abl-du',  romaji: 'vāgbhyām', devanagari: 'वाग्भ्याम्', correctCell: 'cell-abl-du' },
+    { id: 'card-vac-gen-du',  romaji: 'vācoḥ',    devanagari: 'वाचोः',  correctCell: 'cell-gen-du' },
+    { id: 'card-vac-loc-du',  romaji: 'vācoḥ',    devanagari: 'वाचोः',  correctCell: 'cell-loc-du' },
+    { id: 'card-vac-voc-du',  romaji: 'vācau',    devanagari: 'वाचौ',   correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural)
+    { id: 'card-vac-nom-pl',  romaji: 'vācaḥ',    devanagari: 'वाचः',   correctCell: 'cell-nom-pl' },
+    { id: 'card-vac-acc-pl',  romaji: 'vācaḥ',    devanagari: 'वाचः',   correctCell: 'cell-acc-pl' },
+    { id: 'card-vac-ins-pl',  romaji: 'vāgbhiḥ',  devanagari: 'वाग्भिः',  correctCell: 'cell-ins-pl' },
+    { id: 'card-vac-dat-pl',  romaji: 'vāgbhyaḥ', devanagari: 'वाग्भ्यः', correctCell: 'cell-dat-pl' },
+    { id: 'card-vac-abl-pl',  romaji: 'vāgbhyaḥ', devanagari: 'वाग्भ्यः', correctCell: 'cell-abl-pl' },
+    { id: 'card-vac-gen-pl',  romaji: 'vācām',    devanagari: 'वाचाम्',   correctCell: 'cell-gen-pl' },
+    { id: 'card-vac-loc-pl',  romaji: 'vākṣu',     devanagari: 'वाक्षु',    correctCell: 'cell-loc-pl' },
+    { id: 'card-vac-voc-pl',  romaji: 'vācaḥ',    devanagari: 'वाचः',   correctCell: 'cell-voc-pl' }
+];
