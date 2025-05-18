379fd2625167c22db3a9e936a75306605b550b24
kyomtmt <kyowada@gmail.com>
2025-05-18 17:15:09 +0900

    Create tudat-n-data.js
    create tudat N data

---
+++ noun-declension-puzzle/puzzles/tudat-n/tudat-n-data.js
@@ -0,0 +1,33 @@
+// Data for tudat- (n. "striking", present participle) declension puzzle
+
+const tudatNCardsData = [ // "N" for Neuter
+    // 単数 (Singular) - 中性
+    { id: 'card-tudat-n-nom-sg',  romaji: 'tudat',        devanagari: 'तुदत्',       correctCell: 'cell-nom-sg' },
+    { id: 'card-tudat-n-acc-sg',  romaji: 'tudat',        devanagari: 'तुदत्',       correctCell: 'cell-acc-sg' }, // 中性: 主格と同じ
+    { id: 'card-tudat-n-ins-sg',  romaji: 'tudatā',       devanagari: 'तुदता',       correctCell: 'cell-ins-sg' }, // 男性形と同じ
+    { id: 'card-tudat-n-dat-sg',  romaji: 'tudate',       devanagari: 'तुदते',       correctCell: 'cell-dat-sg' }, // 男性形と同じ
+    { id: 'card-tudat-n-abl-sg',  romaji: 'tudataḥ',      devanagari: 'तुदतः',      correctCell: 'cell-abl-sg' }, // 男性形と同じ
+    { id: 'card-tudat-n-gen-sg',  romaji: 'tudataḥ',      devanagari: 'तुदतः',      correctCell: 'cell-gen-sg' }, // 男性形と同じ
+    { id: 'card-tudat-n-loc-sg',  romaji: 'tudati',       devanagari: 'तुदति',       correctCell: 'cell-loc-sg' }, // 男性形と同じ
+    { id: 'card-tudat-n-voc-sg',  romaji: 'tudat',        devanagari: 'तुदत्',       correctCell: 'cell-voc-sg' }, // 中性: 主格と同じ
+
+    // 両数 (Dual) - 中性
+    { id: 'card-tudat-n-nom-du',  romaji: 'tudantī/tudatī', devanagari: 'तुदन्ती/तुदती', correctCell: 'cell-nom-du' },
+    { id: 'card-tudat-n-acc-du',  romaji: 'tudantī/tudatī', devanagari: 'तुदन्ती/तुदती', correctCell: 'cell-acc-du' }, // 中性: 主格と同じ
+    { id: 'card-tudat-n-ins-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-ins-du' }, // 男性形と同じ
+    { id: 'card-tudat-n-dat-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-dat-du' }, // 男性形と同じ
+    { id: 'card-tudat-n-abl-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-abl-du' }, // 男性形と同じ
+    { id: 'card-tudat-n-gen-du',  romaji: 'tudatoḥ',      devanagari: 'तुदतोः',     correctCell: 'cell-gen-du' }, // 男性形と同じ
+    { id: 'card-tudat-n-loc-du',  romaji: 'tudatoḥ',      devanagari: 'तुदतोः',     correctCell: 'cell-loc-du' }, // 男性形と同じ
+    { id: 'card-tudat-n-voc-du',  romaji: 'tudantī/tudatī', devanagari: 'तुदन्ती/तुदती', correctCell: 'cell-voc-du' }, // 中性: 主格と同じ
+
+    // 複数 (Plural) - 中性
+    { id: 'card-tudat-n-nom-pl',  romaji: 'tudanti',      devanagari: 'तुदन्ति',      correctCell: 'cell-nom-pl' },
+    { id: 'card-tudat-n-acc-pl',  romaji: 'tudanti',      devanagari: 'तुदन्ति',      correctCell: 'cell-acc-pl' }, // 中性: 主格と同じ
+    { id: 'card-tudat-n-ins-pl',  romaji: 'tudadbhiḥ',    devanagari: 'तुदद्भिः',    correctCell: 'cell-ins-pl' }, // 男性形と同じ
+    { id: 'card-tudat-n-dat-pl',  romaji: 'tudadbhyaḥ',   devanagari: 'तुदद्भ्यः',   correctCell: 'cell-dat-pl' }, // 男性形と同じ
+    { id: 'card-tudat-n-abl-pl',  romaji: 'tudadbhyaḥ',   devanagari: 'तुदद्भ्यः',   correctCell: 'cell-abl-pl' }, // 男性形と同じ
+    { id: 'card-tudat-n-gen-pl',  romaji: 'tudatām',      devanagari: 'तुदताम्',      correctCell: 'cell-gen-pl' }, // 男性形と同じ
+    { id: 'card-tudat-n-loc-pl',  romaji: 'tudatsu',      devanagari: 'तुदत्सु',      correctCell: 'cell-loc-pl' }, // 男性形と同じ
+    { id: 'card-tudat-n-voc-pl',  romaji: 'tudanti',      devanagari: 'तुदन्ति',      correctCell: 'cell-voc-pl' }  // 中性: 主格と同じ
+];
