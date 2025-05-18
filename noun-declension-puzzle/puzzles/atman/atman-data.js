1039c2dff2fb1a87a7f8181f6effd45de2c5b8c8
kyomtmt <kyowada@gmail.com>
2025-05-18 17:51:07 +0900

    Create atman-data.js
    create atman data

---
+++ noun-declension-puzzle/puzzles/atman/atman-data.js
@@ -0,0 +1,34 @@
+// Data for ātman- (m. "soul, self") declension puzzle
+
+const atmanCardsData = [
+    // 単数 (Singular)
+    { id: 'card-atman-nom-sg',  romaji: 'ātmā',         devanagari: 'आत्मा',        correctCell: 'cell-nom-sg' },
+    { id: 'card-atman-acc-sg',  romaji: 'ātmānam',      devanagari: 'आत्मानम्',     correctCell: 'cell-acc-sg' },
+    { id: 'card-atman-ins-sg',  romaji: 'ātmanā',       devanagari: 'आत्मना',      correctCell: 'cell-ins-sg' },
+    { id: 'card-atman-dat-sg',  romaji: 'ātmane',       devanagari: 'आत्मने',      correctCell: 'cell-dat-sg' },
+    { id: 'card-atman-abl-sg',  romaji: 'ātmanaḥ',      devanagari: 'आत्मनः',     correctCell: 'cell-abl-sg' },
+    { id: 'card-atman-gen-sg',  romaji: 'ātmanaḥ',      devanagari: 'आत्मनः',     correctCell: 'cell-gen-sg' },
+    { id: 'card-atman-loc-sg',  romaji: 'ātmani',       devanagari: 'आत्मनि',      correctCell: 'cell-loc-sg' },
+    { id: 'card-atman-voc-sg',  romaji: 'ātman',        devanagari: 'आत्मन्',       correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    // 表31で空欄の箇所は、一般的なn幹男性名詞のパターンや類似形から補完
+    { id: 'card-atman-nom-du',  romaji: 'ātmānau',      devanagari: 'आत्मानौ',     correctCell: 'cell-nom-du' },
+    { id: 'card-atman-acc-du',  romaji: 'ātmānau',      devanagari: 'आत्मानौ',     correctCell: 'cell-acc-du' },
+    { id: 'card-atman-ins-du',  romaji: 'ātmabhyām',    devanagari: 'आत्मभ्याम्',   correctCell: 'cell-ins-du' },
+    { id: 'card-atman-dat-du',  romaji: 'ātmabhyām',    devanagari: 'आत्मभ्याम्',   correctCell: 'cell-dat-du' },
+    { id: 'card-atman-abl-du',  romaji: 'ātmabhyām',    devanagari: 'आत्मभ्याम्',   correctCell: 'cell-abl-du' },
+    { id: 'card-atman-gen-du',  romaji: 'ātmanoḥ',      devanagari: 'आत्मनोः',    correctCell: 'cell-gen-du' },
+    { id: 'card-atman-loc-du',  romaji: 'ātmanoḥ',      devanagari: 'आत्मनोः',    correctCell: 'cell-loc-du' },
+    { id: 'card-atman-voc-du',  romaji: 'ātmānau',      devanagari: 'आत्मानौ',     correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural)
+    { id: 'card-atman-nom-pl',  romaji: 'ātmānaḥ',      devanagari: 'आत्मानः',     correctCell: 'cell-nom-pl' },
+    { id: 'card-atman-acc-pl',  romaji: 'ātmanaḥ',      devanagari: 'आत्मनः',     correctCell: 'cell-acc-pl' },
+    { id: 'card-atman-ins-pl',  romaji: 'ātmabhiḥ',     devanagari: 'आत्मभिः',    correctCell: 'cell-ins-pl' },
+    { id: 'card-atman-dat-pl',  romaji: 'ātmabhyaḥ',    devanagari: 'आत्मभ्यः',   correctCell: 'cell-dat-pl' },
+    { id: 'card-atman-abl-pl',  romaji: 'ātmabhyaḥ',    devanagari: 'आत्मभ्यः',   correctCell: 'cell-abl-pl' },
+    { id: 'card-atman-gen-pl',  romaji: 'ātmanām',      devanagari: 'आत्मनाम्',     correctCell: 'cell-gen-pl' },
+    { id: 'card-atman-loc-pl',  romaji: 'ātmasu',       devanagari: 'आत्मसु',      correctCell: 'cell-loc-pl' },
+    { id: 'card-atman-voc-pl',  romaji: 'ātmānaḥ',      devanagari: 'आत्मानः',     correctCell: 'cell-voc-pl' }
+];
