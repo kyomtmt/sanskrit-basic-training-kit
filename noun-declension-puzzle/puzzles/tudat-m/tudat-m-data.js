3cc0ebe8fddb8d253f2833061e389fbe53ef2814
kyomtmt <kyowada@gmail.com>
2025-05-18 17:05:52 +0900

    Create tudat-m-data.js
    create tudat-m data

---
+++ noun-declension-puzzle/puzzles/tudat-m/tudat-m-data.js
@@ -0,0 +1,34 @@
+// Data for tudat- (m. "striking", present participle) declension puzzle
+
+const tudatMCardsData = [ // "M" for Masculine
+    // 単数 (Singular) - 男性
+    { id: 'card-tudat-m-nom-sg',  romaji: 'tudan',        devanagari: 'तुदन्',       correctCell: 'cell-nom-sg' },
+    { id: 'card-tudat-m-acc-sg',  romaji: 'tudantam',     devanagari: 'तुदन्तम्',     correctCell: 'cell-acc-sg' },
+    { id: 'card-tudat-m-ins-sg',  romaji: 'tudatā',       devanagari: 'तुदता',       correctCell: 'cell-ins-sg' },
+    { id: 'card-tudat-m-dat-sg',  romaji: 'tudate',       devanagari: 'तुदते',       correctCell: 'cell-dat-sg' },
+    { id: 'card-tudat-m-abl-sg',  romaji: 'tudataḥ',      devanagari: 'तुदतः',      correctCell: 'cell-abl-sg' },
+    { id: 'card-tudat-m-gen-sg',  romaji: 'tudataḥ',      devanagari: 'तुदतः',      correctCell: 'cell-gen-sg' },
+    { id: 'card-tudat-m-loc-sg',  romaji: 'tudati',       devanagari: 'तुदति',       correctCell: 'cell-loc-sg' },
+    { id: 'card-tudat-m-voc-sg',  romaji: 'tudan',        devanagari: 'तुदन्',       correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual) - 男性
+    // 表30で空欄の箇所は、一般的なat語幹男性名詞のパターンや類似形から補完
+    { id: 'card-tudat-m-nom-du',  romaji: 'tudantau',     devanagari: 'तुदन्तौ',     correctCell: 'cell-nom-du' },
+    { id: 'card-tudat-m-acc-du',  romaji: 'tudantau',     devanagari: 'तुदन्तौ',     correctCell: 'cell-acc-du' },
+    { id: 'card-tudat-m-ins-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-ins-du' },
+    { id: 'card-tudat-m-dat-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-dat-du' },
+    { id: 'card-tudat-m-abl-du',  romaji: 'tudadbhyām',   devanagari: 'तुदद्भ्याम्',   correctCell: 'cell-abl-du' },
+    { id: 'card-tudat-m-gen-du',  romaji: 'tudatoḥ',      devanagari: 'तुदतोः',     correctCell: 'cell-gen-du' },
+    { id: 'card-tudat-m-loc-du',  romaji: 'tudatoḥ',      devanagari: 'तुदतोः',     correctCell: 'cell-loc-du' },
+    { id: 'card-tudat-m-voc-du',  romaji: 'tudantau',     devanagari: 'तुदन्तौ',     correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural) - 男性
+    { id: 'card-tudat-m-nom-pl',  romaji: 'tudantaḥ',     devanagari: 'तुदन्तः',     correctCell: 'cell-nom-pl' },
+    { id: 'card-tudat-m-acc-pl',  romaji: 'tudataḥ',      devanagari: 'तुदतः',      correctCell: 'cell-acc-pl' },
+    { id: 'card-tudat-m-ins-pl',  romaji: 'tudadbhiḥ',    devanagari: 'तुदद्भिः',    correctCell: 'cell-ins-pl' },
+    { id: 'card-tudat-m-dat-pl',  romaji: 'tudadbhyaḥ',   devanagari: 'तुदद्भ्यः',   correctCell: 'cell-dat-pl' },
+    { id: 'card-tudat-m-abl-pl',  romaji: 'tudadbhyaḥ',   devanagari: 'तुदद्भ्यः',   correctCell: 'cell-abl-pl' },
+    { id: 'card-tudat-m-gen-pl',  romaji: 'tudatām',      devanagari: 'तुदताम्',      correctCell: 'cell-gen-pl' },
+    { id: 'card-tudat-m-loc-pl',  romaji: 'tudatsu',      devanagari: 'तुदत्सु',      correctCell: 'cell-loc-pl' },
+    { id: 'card-tudat-m-voc-pl',  romaji: 'tudantaḥ',     devanagari: 'तुदन्तः',     correctCell: 'cell-voc-pl' }
+];
