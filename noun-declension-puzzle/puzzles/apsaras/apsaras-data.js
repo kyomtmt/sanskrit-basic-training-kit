bc17bafcf0b23816ad801d47ffda856bedd78195
kyomtmt <kyowada@gmail.com>
2025-05-18 16:26:29 +0900

    Create apsaras-data.js
    create apsaras data

---
+++ noun-declension-puzzle/puzzles/apsaras/apsaras-data.js
@@ -0,0 +1,34 @@
+// Data for apsaras- (f. "celestial nymph") declension puzzle
+
+const apsarasCardsData = [
+    // 単数 (Singular)
+    { id: 'card-apsaras-nom-sg',  romaji: 'apsarāḥ',     devanagari: 'अप्सराः',    correctCell: 'cell-nom-sg' },
+    { id: 'card-apsaras-acc-sg',  romaji: 'apsarasam',   devanagari: 'अप्सरसम्',   correctCell: 'cell-acc-sg' },
+    { id: 'card-apsaras-ins-sg',  romaji: 'apsarasā',    devanagari: 'अप्सरसा',    correctCell: 'cell-ins-sg' },
+    { id: 'card-apsaras-dat-sg',  romaji: 'apsarase',    devanagari: 'अप्सरसे',    correctCell: 'cell-dat-sg' },
+    { id: 'card-apsaras-abl-sg',  romaji: 'apsarasaḥ',   devanagari: 'अप्सरसः',   correctCell: 'cell-abl-sg' },
+    { id: 'card-apsaras-gen-sg',  romaji: 'apsarasaḥ',   devanagari: 'अप्सरसः',   correctCell: 'cell-gen-sg' },
+    { id: 'card-apsaras-loc-sg',  romaji: 'apsarasi',    devanagari: 'अप्सरसि',    correctCell: 'cell-loc-sg' },
+    { id: 'card-apsaras-voc-sg',  romaji: 'apsaraḥ',     devanagari: 'अप्सरः',    correctCell: 'cell-voc-sg' }, 
+
+    // 両数 (Dual)
+    // 表28で空欄の箇所は、一般的なas幹女性名詞のパターンや類似形から補完
+    { id: 'card-apsaras-nom-du',  romaji: 'apsarasau',   devanagari: 'अप्सरसौ',   correctCell: 'cell-nom-du' },
+    { id: 'card-apsaras-acc-du',  romaji: 'apsarasau',   devanagari: 'अप्सरसौ',   correctCell: 'cell-acc-du' },
+    { id: 'card-apsaras-ins-du',  romaji: 'apsarobhyām', devanagari: 'अप्सरोभ्याम्', correctCell: 'cell-ins-du' },
+    { id: 'card-apsaras-dat-du',  romaji: 'apsarobhyām', devanagari: 'अप्सरोभ्याम्', correctCell: 'cell-dat-du' },
+    { id: 'card-apsaras-abl-du',  romaji: 'apsarobhyām', devanagari: 'अप्सरोभ्याम्', correctCell: 'cell-abl-du' },
+    { id: 'card-apsaras-gen-du',  romaji: 'apsarasoḥ',   devanagari: 'अप्सरसोः',  correctCell: 'cell-gen-du' },
+    { id: 'card-apsaras-loc-du',  romaji: 'apsarasoḥ',   devanagari: 'अप्सरसोः',  correctCell: 'cell-loc-du' },
+    { id: 'card-apsaras-voc-du',  romaji: 'apsarasau',   devanagari: 'अप्सरसौ',   correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural)
+    { id: 'card-apsaras-nom-pl',  romaji: 'apsarasaḥ',   devanagari: 'अप्सरसः',   correctCell: 'cell-nom-pl' },
+    { id: 'card-apsaras-acc-pl',  romaji: 'apsarasaḥ',   devanagari: 'अप्सरसः',   correctCell: 'cell-acc-pl' },
+    { id: 'card-apsaras-ins-pl',  romaji: 'apsarobhiḥ',  devanagari: 'अप्सरोभिः',  correctCell: 'cell-ins-pl' },
+    { id: 'card-apsaras-dat-pl',  romaji: 'apsarobhyaḥ', devanagari: 'अप्सरोभ्यः', correctCell: 'cell-dat-pl' },
+    { id: 'card-apsaras-abl-pl',  romaji: 'apsarobhyaḥ', devanagari: 'अप्सरोभ्यः', correctCell: 'cell-abl-pl' }, 
+    { id: 'card-apsaras-gen-pl',  romaji: 'apsarasām',   devanagari: 'अप्सरसाम्',   correctCell: 'cell-gen-pl' },
+    { id: 'card-apsaras-loc-pl',  romaji: 'apsaraḥsu',   devanagari: 'अप्सरःसु',   correctCell: 'cell-loc-pl' },
+    { id: 'card-apsaras-voc-pl',  romaji: 'apsarasaḥ',   devanagari: 'अप्सरसः',   correctCell: 'cell-voc-pl' }
+];
