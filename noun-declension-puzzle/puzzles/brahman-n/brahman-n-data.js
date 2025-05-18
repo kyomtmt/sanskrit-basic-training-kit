b28a914cfff7074744a3682064d019b3b1b7b937
kyomtmt <kyowada@gmail.com>
2025-05-18 17:37:09 +0900

    Create brahman-n-data.js
    create brahma N page

---
+++ noun-declension-puzzle/puzzles/brahman-n/brahman-n-data.js
@@ -0,0 +1,33 @@
+// Data for brahman- (n. "Brahman, the Absolute") declension puzzle
+
+const brahmanNCardsData = [ // "N" for Neuter
+    // 単数 (Singular) - 中性
+    { id: 'card-brahman-n-nom-sg',  romaji: 'brahma',       devanagari: 'ब्रह्म',       correctCell: 'cell-nom-sg' },
+    { id: 'card-brahman-n-acc-sg',  romaji: 'brahma',       devanagari: 'ब्रह्म',       correctCell: 'cell-acc-sg' }, // 中性: 主格と同じ
+    { id: 'card-brahman-n-ins-sg',  romaji: 'brahmaṇā',     devanagari: 'ब्रह्मणा',     correctCell: 'cell-ins-sg' }, // 男性形と同じ
+    { id: 'card-brahman-n-dat-sg',  romaji: 'brahmaṇe',     devanagari: 'ब्रह्मणे',     correctCell: 'cell-dat-sg' }, // 男性形と同じ
+    { id: 'card-brahman-n-abl-sg',  romaji: 'brahmaṇaḥ',    devanagari: 'ब्रह्मणः',    correctCell: 'cell-abl-sg' }, // 男性形と同じ
+    { id: 'card-brahman-n-gen-sg',  romaji: 'brahmaṇaḥ',    devanagari: 'ब्रह्मणः',    correctCell: 'cell-gen-sg' }, // 男性形と同じ
+    { id: 'card-brahman-n-loc-sg',  romaji: 'brahmaṇi',     devanagari: 'ब्रह्मणि',     correctCell: 'cell-loc-sg' }, // 男性形と同じ
+    { id: 'card-brahman-n-voc-sg',  romaji: 'brahma',       devanagari: 'ब्रह्म',       correctCell: 'cell-voc-sg' }, // 中性: 主格と同じ (または brahman も可)
+
+    // 両数 (Dual) - 中性
+    { id: 'card-brahman-n-nom-du',  romaji: 'brahmaṇī',     devanagari: 'ब्रह्मणी',     correctCell: 'cell-nom-du' },
+    { id: 'card-brahman-n-acc-du',  romaji: 'brahmaṇī',     devanagari: 'ब्रह्मणी',     correctCell: 'cell-acc-du' }, // 中性: 主格と同じ
+    { id: 'card-brahman-n-ins-du',  romaji: 'brahmabhyām',  devanagari: 'ब्रह्मभ्याम्',  correctCell: 'cell-ins-du' }, // 男性形と同じ
+    { id: 'card-brahman-n-dat-du',  romaji: 'brahmabhyām',  devanagari: 'ब्रह्मभ्याम्',  correctCell: 'cell-dat-du' }, // 男性形と同じ
+    { id: 'card-brahman-n-abl-du',  romaji: 'brahmabhyām',  devanagari: 'ब्रह्मभ्याम्',  correctCell: 'cell-abl-du' }, // 男性形と同じ
+    { id: 'card-brahman-n-gen-du',  romaji: 'brahmaṇoḥ',    devanagari: 'ब्रह्मणोः',   correctCell: 'cell-gen-du' }, // 男性形と同じ
+    { id: 'card-brahman-n-loc-du',  romaji: 'brahmaṇoḥ',    devanagari: 'ब्रह्मणोः',   correctCell: 'cell-loc-du' }, // 男性形と同じ
+    { id: 'card-brahman-n-voc-du',  romaji: 'brahmaṇī',     devanagari: 'ब्रह्मणी',     correctCell: 'cell-voc-du' }, // 中性: 主格と同じ
+
+    // 複数 (Plural) - 中性
+    { id: 'card-brahman-n-nom-pl',  romaji: 'brahmāṇi',     devanagari: 'ब्रह्माणि',     correctCell: 'cell-nom-pl' },
+    { id: 'card-brahman-n-acc-pl',  romaji: 'brahmāṇi',     devanagari: 'ब्रह्माणि',     correctCell: 'cell-acc-pl' }, // 中性: 主格と同じ
+    { id: 'card-brahman-n-ins-pl',  romaji: 'brahmabhiḥ',   devanagari: 'ब्रह्मभिः',   correctCell: 'cell-ins-pl' }, // 男性形と同じ
+    { id: 'card-brahman-n-dat-pl',  romaji: 'brahmabhyaḥ',  devanagari: 'ब्रह्मभ्यः',  correctCell: 'cell-dat-pl' }, // 男性形と同じ
+    { id: 'card-brahman-n-abl-pl',  romaji: 'brahmabhyaḥ',  devanagari: 'ब्रह्मभ्यः',  correctCell: 'cell-abl-pl' }, // 男性形と同じ
+    { id: 'card-brahman-n-gen-pl',  romaji: 'brahmaṇām',    devanagari: 'ब्रह्मणाम्',   correctCell: 'cell-gen-pl' }, // 男性形と同じ
+    { id: 'card-brahman-n-loc-pl',  romaji: 'brahmasu',     devanagari: 'ब्रह्मसु',     correctCell: 'cell-loc-pl' }, // 男性形と同じ
+    { id: 'card-brahman-n-voc-pl',  romaji: 'brahmāṇi',     devanagari: 'ब्रह्माणि',     correctCell: 'cell-voc-pl' }  // 中性: 主格と同じ
+];
