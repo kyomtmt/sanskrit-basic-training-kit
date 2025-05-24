926e0f854c55b894d42046b0f83154b35e959d91
kyomtmt <kyowada@gmail.com>
2025-05-24 20:48:26 +0900

    Create dana-data.js

---
+++ noun-declension-puzzle/puzzles/dana/dana-data.js
@@ -0,0 +1,36 @@
+// Data for dāna (n.) declension puzzle
+
+const danaCardsData = [
+    // 単数 (Singular)
+    { id: 'card-dana-nom-sg',  romaji: 'dānam',    devanagari: 'दानम्',   correctCell: 'cell-nom-sg' }, // N. Ac. sg. dānam
+    { id: 'card-dana-acc-sg',  romaji: 'dānam',    devanagari: 'दानम्',   correctCell: 'cell-acc-sg' }, // N. Ac. sg. dānam
+    { id: 'card-dana-ins-sg',  romaji: 'dānena',   devanagari: 'दानेन',   correctCell: 'cell-ins-sg' }, // aśvena と同様
+    { id: 'card-dana-dat-sg',  romaji: 'dānāya',   devanagari: 'दानाय',   correctCell: 'cell-dat-sg' }, // aśvāya と同様
+    { id: 'card-dana-abl-sg',  romaji: 'dānāt',    devanagari: 'दानात्',   correctCell: 'cell-abl-sg' }, // aśvāt と同様
+    { id: 'card-dana-gen-sg',  romaji: 'dānasya',  devanagari: 'दानस्य',  correctCell: 'cell-gen-sg' }, // aśvasya と同様
+    { id: 'card-dana-loc-sg',  romaji: 'dāne',     devanagari: 'दाने',    correctCell: 'cell-loc-sg' }, // aśve と同様
+    { id: 'card-dana-voc-sg',  romaji: 'dāna',     devanagari: 'दान',    correctCell: 'cell-voc-sg' }, // V. dāna
+
+    // 両数 (Dual)
+    { id: 'card-dana-nom-du',  romaji: 'dāne',     devanagari: 'दाने',    correctCell: 'cell-nom-du' }, // N. V. Ac. du. dāne
+    { id: 'card-dana-acc-du',  romaji: 'dāne',     devanagari: 'दाने',    correctCell: 'cell-acc-du' }, // N. V. Ac. du. dāne
+    { id: 'card-dana-ins-du',  romaji: 'dānābhyām',devanagari: 'दानाभ्याम्',correctCell: 'cell-ins-du' }, // aśvābhyām と同様
+    { id: 'card-dana-dat-du',  romaji: 'dānābhyām',devanagari: 'दानाभ्याम्',correctCell: 'cell-dat-du' }, // aśvābhyām と同様
+    { id: 'card-dana-abl-du',  romaji: 'dānābhyām',devanagari: 'दानाभ्याम्',correctCell: 'cell-abl-du' }, // aśvābhyām と同様
+    { id: 'card-dana-gen-du',  romaji: 'dānayoḥ',  devanagari: 'दानयोः',  correctCell: 'cell-gen-du' }, // aśvayoḥ と同様
+    { id: 'card-dana-loc-du',  romaji: 'dānayoḥ',  devanagari: 'दानयोः',  correctCell: 'cell-loc-du' }, // aśvayoḥ と同様
+    { id: 'card-dana-voc-du',  romaji: 'dāne',     devanagari: 'दाने',    correctCell: 'cell-voc-du' }, // N. V. Ac. du. dāne
+
+    // 複数 (Plural)
+    { id: 'card-dana-nom-pl',  romaji: 'dānāni',   devanagari: 'दानानि',  correctCell: 'cell-nom-pl' }, // N. V. Ac. pl. dānāni
+    { id: 'card-dana-acc-pl',  romaji: 'dānāni',   devanagari: 'दानानि',  correctCell: 'cell-acc-pl' }, // N. V. Ac. pl. dānāni
+    { id: 'card-dana-ins-pl',  romaji: 'dānaiḥ',   devanagari: 'दानैः',   correctCell: 'cell-ins-pl' }, // aśvaiḥ と同様
+    { id: 'card-dana-dat-pl',  romaji: 'dānebhyaḥ',devanagari: 'दानेभ्यः', correctCell: 'cell-dat-pl' }, // aśvebhyaḥ と同様
+    { id: 'card-dana-abl-pl',  romaji: 'dānebhyaḥ',devanagari: 'दानेभ्यः', correctCell: 'cell-abl-pl' }, // aśvebhyaḥ と同様
+    { id: 'card-dana-gen-pl',  romaji: 'dānānām',  devanagari: 'दानानाम्',  correctCell: 'cell-gen-pl' }, // aśvānām と同様
+    { id: 'card-dana-loc-pl',  romaji: 'dāneṣu',   devanagari: 'दानेषु',   correctCell: 'cell-loc-pl' }, // aśveṣu と同様
+    { id: 'card-dana-voc-pl',  romaji: 'dānāni',   devanagari: 'दानानि',  correctCell: 'cell-voc-pl' }  // N. V. Ac. pl. dānāni
+];
+
+// console.log("dana-data.js loaded");
+// console.log(danaCardsData);
