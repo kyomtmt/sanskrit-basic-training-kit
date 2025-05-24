42669c74cdead6b109f1a249b2ee21984eedbb6c
kyomtmt <kyowada@gmail.com>
2025-05-24 20:39:25 +0900

    Create kanya-data.js

---
+++ noun-declension-puzzle/puzzles/kanya/kanya-data.js
@@ -0,0 +1,36 @@
+// Data for kanyā (f.) declension puzzle
+
+const kanyaCardsData = [
+    // 単数 (Singular)
+    { id: 'card-kanya-nom-sg',  romaji: 'kanyā',    devanagari: 'कन्या',   correctCell: 'cell-nom-sg' },
+    { id: 'card-kanya-acc-sg',  romaji: 'kanyām',   devanagari: 'कन्याम्',  correctCell: 'cell-acc-sg' },
+    { id: 'card-kanya-ins-sg',  romaji: 'kanyayā',  devanagari: 'कन्यया',  correctCell: 'cell-ins-sg' },
+    { id: 'card-kanya-dat-sg',  romaji: 'kanyāyai', devanagari: 'कन्यायै', correctCell: 'cell-dat-sg' },
+    { id: 'card-kanya-abl-sg',  romaji: 'kanyāyāḥ', devanagari: 'कन्यायाः', correctCell: 'cell-abl-sg' },
+    { id: 'card-kanya-gen-sg',  romaji: 'kanyāyāḥ', devanagari: 'कन्यायाः', correctCell: 'cell-gen-sg' },
+    { id: 'card-kanya-loc-sg',  romaji: 'kanyāyām', devanagari: 'कन्यायाम्', correctCell: 'cell-loc-sg' },
+    { id: 'card-kanya-voc-sg',  romaji: 'kanye',    devanagari: 'कन्ये',    correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    { id: 'card-kanya-nom-du',  romaji: 'kanye',    devanagari: 'कन्ये',    correctCell: 'cell-nom-du' },
+    { id: 'card-kanya-acc-du',  romaji: 'kanye',    devanagari: 'कन्ये',    correctCell: 'cell-acc-du' },
+    { id: 'card-kanya-ins-du',  romaji: 'kanyābhyām',devanagari: 'कन्याभ्याम्',correctCell: 'cell-ins-du' },
+    { id: 'card-kanya-dat-du',  romaji: 'kanyābhyām',devanagari: 'कन्याभ्याम्',correctCell: 'cell-dat-du' },
+    { id: 'card-kanya-abl-du',  romaji: 'kanyābhyām',devanagari: 'कन्याभ्याम्',correctCell: 'cell-abl-du' },
+    { id: 'card-kanya-gen-du',  romaji: 'kanyayoḥ', devanagari: 'कन्ययोः',  correctCell: 'cell-gen-du' },
+    { id: 'card-kanya-loc-du',  romaji: 'kanyayoḥ', devanagari: 'कन्ययोः',  correctCell: 'cell-loc-du' },
+    { id: 'card-kanya-voc-du',  romaji: 'kanye',    devanagari: 'कन्ये',    correctCell: 'cell-voc-du' }, // 活用表では "=N."
+
+    // 複数 (Plural)
+    { id: 'card-kanya-nom-pl',  romaji: 'kanyāḥ',   devanagari: 'कन्याः',   correctCell: 'cell-nom-pl' },
+    { id: 'card-kanya-acc-pl',  romaji: 'kanyāḥ',   devanagari: 'कन्याः',   correctCell: 'cell-acc-pl' }, // 活用表によると対格複数は主格複数と同じ形のようです
+    { id: 'card-kanya-ins-pl',  romaji: 'kanyābhiḥ',devanagari: 'कन्याभिः', correctCell: 'cell-ins-pl' },
+    { id: 'card-kanya-dat-pl',  romaji: 'kanyābhyaḥ',devanagari: 'कन्याभ्यः',correctCell: 'cell-dat-pl' },
+    { id: 'card-kanya-abl-pl',  romaji: 'kanyābhyaḥ',devanagari: 'कन्याभ्यः',correctCell: 'cell-abl-pl' },
+    { id: 'card-kanya-gen-pl',  romaji: 'kanyānām', devanagari: 'कन्यानाम्',  correctCell: 'cell-gen-pl' },
+    { id: 'card-kanya-loc-pl',  romaji: 'kanyāsu',  devanagari: 'कन्यासु',   correctCell: 'cell-loc-pl' },
+    { id: 'card-kanya-voc-pl',  romaji: 'kanyāḥ',   devanagari: 'कन्याः',   correctCell: 'cell-voc-pl' }  // 活用表では "=N."
+];
+
+// console.log("kanya-data.js loaded");
+// console.log(kanyaCardsData);
