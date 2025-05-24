3fcde987763c6313c9d7c97367ec093eaebff2fd
kyomtmt <kyowada@gmail.com>
2025-05-24 22:13:21 +0900

    Update pasu-data.js

--- noun-declension-puzzle/puzzles/pasu/pasu-data.js
+++ noun-declension-puzzle/puzzles/pasu/pasu-data.js
@@ -1,36 +1,43 @@
-// Data for paśu (m.) declension puzzle (従来式)
+// Data for paśu (m.) declension puzzle
 
 const pasuCardsData = [
     // 単数 (Singular)
-    { id: 'card-pasu-nom-sg',  romaji: 'paśuḥ',    devanagari: 'पशुः',   correctCell: 'cell-nom-sg' },
-    { id: 'card-pasu-acc-sg',  romaji: 'paśum',    devanagari: 'पशुम्',   correctCell: 'cell-acc-sg' },
-    { id: 'card-pasu-ins-sg',  romaji: 'paśunā',   devanagari: 'पशुना',   correctCell: 'cell-ins-sg' },
-    { id: 'card-pasu-dat-sg',  romaji: 'paśave',   devanagari: 'पशवे',   correctCell: 'cell-dat-sg' },
-    { id: 'card-pasu-abl-sg',  romaji: 'paśoḥ',    devanagari: 'पशोः',    correctCell: 'cell-abl-sg' }, // 活用表では paśoḥ
-    { id: 'card-pasu-gen-sg',  romaji: 'paśoḥ',    devanagari: 'पशोः',    correctCell: 'cell-gen-sg' }, // 活用表では paśoḥ
-    { id: 'card-pasu-loc-sg',  romaji: 'paśau',    devanagari: 'पशौ',    correctCell: 'cell-loc-sg' },
-    { id: 'card-pasu-voc-sg',  romaji: 'paśo',     devanagari: 'पशो',     correctCell: 'cell-voc-sg' }, // 活用表では paśo
+    { id: 'pasu-nom-sg',  romaji: 'paśuḥ',    devanagari: 'पशुः',   acceptableCells: ['cell-nom-sg'] },
+    { id: 'pasu-acc-sg',  romaji: 'paśum',    devanagari: 'पशुम्',   acceptableCells: ['cell-acc-sg'] },
+    { id: 'pasu-ins-sg',  romaji: 'paśunā',   devanagari: 'पशुना',   acceptableCells: ['cell-ins-sg'] },
+    { id: 'pasu-dat-sg',  romaji: 'paśave',   devanagari: 'पशवे',   acceptableCells: ['cell-dat-sg'] },
+    // 奪格・属格が同じ paśoḥ なので、対応するカードを2枚用意し、acceptableCells を使う
+    { id: 'pasu-ablgen-sg-1', romaji: 'paśoḥ',    devanagari: 'पशोः',    acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'pasu-ablgen-sg-2', romaji: 'paśoḥ',    devanagari: 'पशोः',    acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'pasu-loc-sg',  romaji: 'paśau',    devanagari: 'पशौ',    acceptableCells: ['cell-loc-sg'] },
+    { id: 'pasu-voc-sg',  romaji: 'paśo',     devanagari: 'पशो',     acceptableCells: ['cell-voc-sg'] },
 
     // 両数 (Dual)
-    { id: 'card-pasu-nom-du',  romaji: 'paśū',     devanagari: 'पशू',     correctCell: 'cell-nom-du' }, // N.Ac.V.du. paśū (長母音)
-    { id: 'card-pasu-acc-du',  romaji: 'paśū',     devanagari: 'पशू',     correctCell: 'cell-acc-du' }, // N.Ac.V.du. paśū (長母音)
-    { id: 'card-pasu-ins-du',  romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', correctCell: 'cell-ins-du' },
-    { id: 'card-pasu-dat-du',  romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', correctCell: 'cell-dat-du' },
-    { id: 'card-pasu-abl-du',  romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', correctCell: 'cell-abl-du' },
-    { id: 'card-pasu-gen-du',  romaji: 'paśvoḥ',   devanagari: 'पश्वोः',   correctCell: 'cell-gen-du' },
-    { id: 'card-pasu-loc-du',  romaji: 'paśvoḥ',   devanagari: 'पश्वोः',   correctCell: 'cell-loc-du' },
-    { id: 'card-pasu-voc-du',  romaji: 'paśū',     devanagari: 'पशू',     correctCell: 'cell-voc-du' }, // N.Ac.V.du. paśū (=N.)
+    // 主格・対格・呼格が同じ paśū なので、対応するカードを3枚用意し、acceptableCells を使う
+    { id: 'pasu-nav-du-1', romaji: 'paśū',     devanagari: 'पशू',     acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'pasu-nav-du-2', romaji: 'paśū',     devanagari: 'पशू',     acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'pasu-nav-du-3', romaji: 'paśū',     devanagari: 'पशू',     acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    // ★★★ 具格・与格・奪格が同じ paśubhyām なので、対応するカードを3枚用意し、acceptableCells を使う ★★★
+    { id: 'pasu-ida-du-1', romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'pasu-ida-du-2', romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'pasu-ida-du-3', romaji: 'paśubhyām',devanagari: 'पशुभ्याम्', acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    // 属格・処格が同じ paśvoḥ なので、対応するカードを2枚用意し、acceptableCells を使う
+    { id: 'pasu-gl-du-1',  romaji: 'paśvoḥ',   devanagari: 'पश्वोः',   acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
+    { id: 'pasu-gl-du-2',  romaji: 'paśvoḥ',   devanagari: 'पश्वोः',   acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
 
     // 複数 (Plural)
-    { id: 'card-pasu-nom-pl',  romaji: 'paśavaḥ',  devanagari: 'पशवः',   correctCell: 'cell-nom-pl' },
-    { id: 'card-pasu-acc-pl',  romaji: 'paśūn',    devanagari: 'पशून्',   correctCell: 'cell-acc-pl' }, // 活用表では paśūn (長母音)
-    { id: 'card-pasu-ins-pl',  romaji: 'paśubhiḥ', devanagari: 'पशुभिः',  correctCell: 'cell-ins-pl' },
-    { id: 'card-pasu-dat-pl',  romaji: 'paśubhyaḥ',devanagari: 'पशुभ्यः',  correctCell: 'cell-dat-pl' },
-    { id: 'card-pasu-abl-pl',  romaji: 'paśubhyaḥ',devanagari: 'पशुभ्यः',  correctCell: 'cell-abl-pl' },
-    { id: 'card-pasu-gen-pl',  romaji: 'paśūnām',  devanagari: 'पशूनाम्',  correctCell: 'cell-gen-pl' },
-    { id: 'card-pasu-loc-pl',  romaji: 'paśuṣu',   devanagari: 'पशुषु',   correctCell: 'cell-loc-pl' },
-    { id: 'card-pasu-voc-pl',  romaji: 'paśavaḥ',  devanagari: 'पशवः',   correctCell: 'cell-voc-pl' }  // (=N.)
+    // 主格・呼格が同じ paśavaḥ なので、対応するカードを2枚用意し、acceptableCells を使う
+    { id: 'pasu-nav-pl-1', romaji: 'paśavaḥ',  devanagari: 'पशवः',   acceptableCells: ['cell-nom-pl', 'cell-voc-pl'] },
+    { id: 'pasu-nav-pl-2', romaji: 'paśavaḥ',  devanagari: 'पशवः',   acceptableCells: ['cell-nom-pl', 'cell-voc-pl'] },
+    { id: 'pasu-acc-pl', romaji: 'paśūn',    devanagari: 'पशून्',   acceptableCells: ['cell-acc-pl'] },
+    { id: 'pasu-ins-pl', romaji: 'paśubhiḥ', devanagari: 'पशुभिः',  acceptableCells: ['cell-ins-pl'] },
+    // 与格・奪格が同じ paśubhyaḥ なので、対応するカードを2枚用意し、acceptableCells を使う
+    { id: 'pasu-da-pl-1',  romaji: 'paśubhyaḥ',devanagari: 'पशुभ्यः',  acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'pasu-da-pl-2',  romaji: 'paśubhyaḥ',devanagari: 'पशुभ्यः',  acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'pasu-gen-pl', romaji: 'paśūnām',  devanagari: 'पशूनाम्',  acceptableCells: ['cell-gen-pl'] },
+    { id: 'pasu-loc-pl', romaji: 'paśuṣu',   devanagari: 'पशुषु',   acceptableCells: ['cell-loc-pl'] }
 ];
+// カードの総数が24枚になるようにIDを変えて定義しました。
 
 // console.log("pasu-data.js loaded");
 // console.log(pasuCardsData);
