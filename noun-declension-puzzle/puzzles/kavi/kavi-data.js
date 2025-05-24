044ed8c25bb84bf2b6e7c27343adfc6ea5197ce5
kyomtmt <kyowada@gmail.com>
2025-05-24 21:05:52 +0900

    Create kavi-data.js

---
+++ noun-declension-puzzle/puzzles/kavi/kavi-data.js
@@ -0,0 +1,36 @@
+// Data for kavi (m.) declension puzzle
+
+const kaviCardsData = [
+    // 単数 (Singular)
+    { id: 'card-kavi-nom-sg',  romaji: 'kaviḥ',    devanagari: 'कविः',   correctCell: 'cell-nom-sg' },
+    { id: 'card-kavi-acc-sg',  romaji: 'kavim',    devanagari: 'कविम्',   correctCell: 'cell-acc-sg' },
+    { id: 'card-kavi-ins-sg',  romaji: 'kavinā',   devanagari: 'कविना',   correctCell: 'cell-ins-sg' },
+    { id: 'card-kavi-dat-sg',  romaji: 'kavaye',   devanagari: 'कवये',   correctCell: 'cell-dat-sg' },
+    { id: 'card-kavi-abl-sg',  romaji: 'kaveḥ',    devanagari: 'कवेः',    correctCell: 'cell-abl-sg' }, // 活用表では kaveḥ
+    { id: 'card-kavi-gen-sg',  romaji: 'kaveḥ',    devanagari: 'कवेः',    correctCell: 'cell-gen-sg' }, // 活用表では kaveḥ
+    { id: 'card-kavi-loc-sg',  romaji: 'kavau',    devanagari: 'कवौ',    correctCell: 'cell-loc-sg' },
+    { id: 'card-kavi-voc-sg',  romaji: 'kave',     devanagari: 'कवे',     correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    { id: 'card-kavi-nom-du',  romaji: 'kavī',     devanagari: 'कवी',     correctCell: 'cell-nom-du' }, // 活用表では kavī (長母音)
+    { id: 'card-kavi-acc-du',  romaji: 'kavī',     devanagari: 'कवी',     correctCell: 'cell-acc-du' }, // 活用表では kavī (長母音)
+    { id: 'card-kavi-ins-du',  romaji: 'kavibhyām',devanagari: 'कविभ्याम्', correctCell: 'cell-ins-du' },
+    { id: 'card-kavi-dat-du',  romaji: 'kavibhyām',devanagari: 'कविभ्याम्', correctCell: 'cell-dat-du' },
+    { id: 'card-kavi-abl-du',  romaji: 'kavibhyām',devanagari: 'कविभ्याम्', correctCell: 'cell-abl-du' },
+    { id: 'card-kavi-gen-du',  romaji: 'kavyoḥ',   devanagari: 'कव्योः',  correctCell: 'cell-gen-du' },
+    { id: 'card-kavi-loc-du',  romaji: 'kavyoḥ',   devanagari: 'कव्योः',  correctCell: 'cell-loc-du' },
+    { id: 'card-kavi-voc-du',  romaji: 'kavī',     devanagari: 'कवी',     correctCell: 'cell-voc-du' }, // 活用表では "=N." (kavī)
+
+    // 複数 (Plural)
+    { id: 'card-kavi-nom-pl',  romaji: 'kavayaḥ',  devanagari: 'कवयः',  correctCell: 'cell-nom-pl' },
+    { id: 'card-kavi-acc-pl',  romaji: 'kavīn',    devanagari: 'कवीन्',   correctCell: 'cell-acc-pl' }, // 活用表では kavīn (長母音)
+    { id: 'card-kavi-ins-pl',  romaji: 'kavibhiḥ', devanagari: 'कविभिः', correctCell: 'cell-ins-pl' },
+    { id: 'card-kavi-dat-pl',  romaji: 'kavibhyaḥ',devanagari: 'कविभ्यः', correctCell: 'cell-dat-pl' },
+    { id: 'card-kavi-abl-pl',  romaji: 'kavibhyaḥ',devanagari: 'कविभ्यः', correctCell: 'cell-abl-pl' },
+    { id: 'card-kavi-gen-pl',  romaji: 'kavīnām',  devanagari: 'कवीनाम्',  correctCell: 'cell-gen-pl' },
+    { id: 'card-kavi-loc-pl',  romaji: 'kaviṣu',   devanagari: 'कविषु',   correctCell: 'cell-loc-pl' },
+    { id: 'card-kavi-voc-pl',  romaji: 'kavayaḥ',  devanagari: 'कवयः',  correctCell: 'cell-voc-pl' }  // 活用表では "=N." (kavayaḥ)
+];
+
+// console.log("kavi-data.js loaded");
+// console.log(kaviCardsData);
