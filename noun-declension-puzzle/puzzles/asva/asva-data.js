780a0b974c1a3efdd78dd082f27c9e71e9652e07
kyomtmt <kyowada@gmail.com>
2025-05-24 20:26:46 +0900

    Create asva-data.js

---
+++ noun-declension-puzzle/puzzles/asva/asva-data.js
@@ -0,0 +1,36 @@
+// Data for aśva declension puzzle
+
+const asvaCardsData = [ // 変数名から M を削除
+    // 単数 (Singular)
+    { id: 'card-asva-nom-sg',  romaji: 'aśvaḥ',    devanagari: 'अश्वः',   correctCell: 'cell-nom-sg' }, // idから -m を削除
+    { id: 'card-asva-acc-sg',  romaji: 'aśvam',    devanagari: 'अश्वम्',  correctCell: 'cell-acc-sg' }, // 以下同様にidから -m を削除
+    { id: 'card-asva-ins-sg',  romaji: 'aśvena',   devanagari: 'अश्वेन',  correctCell: 'cell-ins-sg' },
+    { id: 'card-asva-dat-sg',  romaji: 'aśvāya',   devanagari: 'अश्वाय',  correctCell: 'cell-dat-sg' },
+    { id: 'card-asva-abl-sg',  romaji: 'aśvāt',    devanagari: 'अश्वात्',  correctCell: 'cell-abl-sg' },
+    { id: 'card-asva-gen-sg',  romaji: 'aśvasya',  devanagari: 'अश्वस्य', correctCell: 'cell-gen-sg' },
+    { id: 'card-asva-loc-sg',  romaji: 'aśve',     devanagari: 'अश्वे',    correctCell: 'cell-loc-sg' },
+    { id: 'card-asva-voc-sg',  romaji: 'aśva',     devanagari: 'अश्व',    correctCell: 'cell-voc-sg' },
+
+    // 両数 (Dual)
+    { id: 'card-asva-nom-du',  romaji: 'aśvau',    devanagari: 'अश्वौ',   correctCell: 'cell-nom-du' },
+    { id: 'card-asva-acc-du',  romaji: 'aśvau',    devanagari: 'अश्वौ',   correctCell: 'cell-acc-du' },
+    { id: 'card-asva-ins-du',  romaji: 'aśvābhyām',devanagari: 'अश्वाभ्याम्',correctCell: 'cell-ins-du' },
+    { id: 'card-asva-dat-du',  romaji: 'aśvābhyām',devanagari: 'अश्वाभ्याम्',correctCell: 'cell-dat-du' },
+    { id: 'card-asva-abl-du',  romaji: 'aśvābhyām',devanagari: 'अश्वाभ्याम्',correctCell: 'cell-abl-du' },
+    { id: 'card-asva-gen-du',  romaji: 'aśvayoḥ',  devanagari: 'अश्वयोः', correctCell: 'cell-gen-du' },
+    { id: 'card-asva-loc-du',  romaji: 'aśvayoḥ',  devanagari: 'अश्वयोः', correctCell: 'cell-loc-du' },
+    { id: 'card-asva-voc-du',  romaji: 'aśvau',    devanagari: 'अश्वौ',   correctCell: 'cell-voc-du' },
+
+    // 複数 (Plural)
+    { id: 'card-asva-nom-pl',  romaji: 'aśvāḥ',    devanagari: 'अश्वाः',  correctCell: 'cell-nom-pl' },
+    { id: 'card-asva-acc-pl',  romaji: 'aśvān',    devanagari: 'अश्वान्',  correctCell: 'cell-acc-pl' },
+    { id: 'card-asva-ins-pl',  romaji: 'aśvaiḥ',   devanagari: 'अश्वैः',  correctCell: 'cell-ins-pl' },
+    { id: 'card-asva-dat-pl',  romaji: 'aśvebhyaḥ',devanagari: 'अश्वेभ्यः',correctCell: 'cell-dat-pl' },
+    { id: 'card-asva-abl-pl',  romaji: 'aśvebhyaḥ',devanagari: 'अश्वेभ्यः',correctCell: 'cell-abl-pl' },
+    { id: 'card-asva-gen-pl',  romaji: 'aśvānām',  devanagari: 'अश्वानाम्', correctCell: 'cell-gen-pl' },
+    { id: 'card-asva-loc-pl',  romaji: 'aśveṣu',   devanagari: 'अश्वेषु',  correctCell: 'cell-loc-pl' },
+    { id: 'card-asva-voc-pl',  romaji: 'aśvāḥ',    devanagari: 'अश्वाः',  correctCell: 'cell-voc-pl' }
+];
+
+// console.log("asva-data.js loaded");
+// console.log(asvaCardsData);
