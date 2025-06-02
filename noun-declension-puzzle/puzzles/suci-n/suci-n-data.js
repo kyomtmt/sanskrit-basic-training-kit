a2f328a5435bf484f4166431b8f160f35c1d96f4
kyomtmt <kyowada@gmail.com>
2025-06-03 08:50:44 +0900

    Create suci-n-data.js

---
+++ noun-declension-puzzle/puzzles/suci-n/suci-n-data.js
@@ -0,0 +1,38 @@
+// Data for śuci- (n.) declension puzzle (清い - 中性) - 
+
+const suciNCardsData = [
+    // 単数 (Singular) - 中性
+    { id: 'suci-n-na-sg-1', romaji: 'śuci',           devanagari: 'शुचि',         acceptableCells: ['cell-nom-sg', 'cell-acc-sg'] },
+    { id: 'suci-n-na-sg-2', romaji: 'śuci',           devanagari: 'शुचि',         acceptableCells: ['cell-nom-sg', 'cell-acc-sg'] },
+    { id: 'suci-n-ins-sg',  romaji: 'śucinā',         devanagari: 'शुचिना',       acceptableCells: ['cell-ins-sg'] },
+    { id: 'suci-n-dat-sg',  romaji: 'śucine/śucaye',  devanagari: 'शुचिने/शुचये', acceptableCells: ['cell-dat-sg'] },
+    { id: 'suci-n-ablgen-sg-1', romaji: 'śucinaḥ/śuceḥ',devanagari: 'शुचिनः/शुचेः',acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'suci-n-ablgen-sg-2', romaji: 'śucinaḥ/śuceḥ',devanagari: 'शुचिनः/शुचेः',acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'suci-n-loc-sg',  romaji: 'śucini/śucau',   devanagari: 'शुचिनि/शुचौ',  acceptableCells: ['cell-loc-sg'] },
+    { id: 'suci-n-voc-sg',  romaji: 'śuci/śuce',      devanagari: 'शुचि/शुचे',    acceptableCells: ['cell-voc-sg'] },
+
+    // 両数 (Dual) - 中性
+    // ★★★ 主格・対格・呼格を śucinī に戻す ★★★
+    { id: 'suci-n-nav-du-1', romaji: 'śucinī',         devanagari: 'शुचिनी',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'suci-n-nav-du-2', romaji: 'śucinī',         devanagari: 'शुचिनी',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'suci-n-nav-du-3', romaji: 'śucinī',         devanagari: 'शुचिनी',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'suci-n-ida-du-1', romaji: 'śucibhyām',      devanagari: 'शुचिभ्याम्',    acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'suci-n-ida-du-2', romaji: 'śucibhyām',      devanagari: 'शुचिभ्याम्',    acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'suci-n-ida-du-3', romaji: 'śucibhyām',      devanagari: 'शुचिभ्याम्',    acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'suci-n-gl-du-1',  romaji: 'śucinoḥ/śucyoḥ', devanagari: 'शुचिनोः/शुच्योः',acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
+    { id: 'suci-n-gl-du-2',  romaji: 'śucinoḥ/śucyoḥ', devanagari: 'शुचिनोः/शुच्योः',acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
+
+    // 複数 (Plural) - 中性
+    { id: 'suci-n-nav-pl-1', romaji: 'śucīni',         devanagari: 'शुचीनि',       acceptableCells: ['cell-nom-pl', 'cell-acc-pl', 'cell-voc-pl'] },
+    { id: 'suci-n-nav-pl-2', romaji: 'śucīni',         devanagari: 'शुचीनि',       acceptableCells: ['cell-nom-pl', 'cell-acc-pl', 'cell-voc-pl'] },
+    { id: 'suci-n-nav-pl-3', romaji: 'śucīni',         devanagari: 'शुचीनि',       acceptableCells: ['cell-nom-pl', 'cell-acc-pl', 'cell-voc-pl'] },
+    { id: 'suci-n-ins-pl', romaji: 'śucibhiḥ',       devanagari: 'शुचिभिः',      acceptableCells: ['cell-ins-pl'] },
+    { id: 'suci-n-da-pl-1',  romaji: 'śucibhyaḥ',      devanagari: 'शुचिभ्यः',      acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'suci-n-da-pl-2',  romaji: 'śucibhyaḥ',      devanagari: 'शुचिभ्यः',      acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'suci-n-gen-pl', romaji: 'śucīnām',        devanagari: 'शुचीनाम्',      acceptableCells: ['cell-gen-pl'] },
+    { id: 'suci-n-loc-pl', romaji: 'śuciṣu',         devanagari: 'शुचिषु',        acceptableCells: ['cell-loc-pl'] }
+];
+
+// console.log("suci-n-data.js loaded");
+// console.log(suciNCardsData.length);
+// console.log(suciNCardsData);
