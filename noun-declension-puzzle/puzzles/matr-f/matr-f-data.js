7a2f3dd194e3e1b8a9319068888301e3bb0ace6a
kyomtmt <kyowada@gmail.com>
2025-06-03 19:19:45 +0900

    Create matr-f-data.js

---
+++ noun-declension-puzzle/puzzles/matr-f/matr-f-data.js
@@ -0,0 +1,36 @@
+// Data for mātṛ- (f.) declension puzzle (母 - 女性)
+
+const matrFCardsData = [
+    // 単数 (Singular)
+    { id: 'matr-f-nom-sg',  romaji: 'mātā',           devanagari: 'माता',          acceptableCells: ['cell-nom-sg'] },
+    { id: 'matr-f-acc-sg',  romaji: 'mātaram',        devanagari: 'मातरम्',       acceptableCells: ['cell-acc-sg'] },
+    { id: 'matr-f-ins-sg',  romaji: 'mātrā',          devanagari: 'मात्रा',         acceptableCells: ['cell-ins-sg'] },
+    { id: 'matr-f-dat-sg',  romaji: 'mātre',          devanagari: 'मात्रे',         acceptableCells: ['cell-dat-sg'] },
+    { id: 'matr-f-ablgen-sg-1', romaji: 'mātuḥ',          devanagari: 'मातुः',        acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'matr-f-ablgen-sg-2', romaji: 'mātuḥ',          devanagari: 'मातुः',        acceptableCells: ['cell-abl-sg', 'cell-gen-sg'] },
+    { id: 'matr-f-loc-sg',  romaji: 'mātari',         devanagari: 'मातरि',        acceptableCells: ['cell-loc-sg'] },
+    { id: 'matr-f-voc-sg',  romaji: 'mātaḥ (< mātar)',devanagari: 'मातः (< मातर्)', acceptableCells: ['cell-voc-sg'] },
+
+    // 両数 (Dual)
+    { id: 'matr-f-nav-du-1', romaji: 'mātarau',        devanagari: 'मातरौ',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'matr-f-nav-du-2', romaji: 'mātarau',        devanagari: 'मातरौ',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'matr-f-nav-du-3', romaji: 'mātarau',        devanagari: 'मातरौ',       acceptableCells: ['cell-nom-du', 'cell-acc-du', 'cell-voc-du'] },
+    { id: 'matr-f-ida-du-1', romaji: 'mātṛbhyām',      devanagari: 'मातृभ्याम्',     acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'matr-f-ida-du-2', romaji: 'mātṛbhyām',      devanagari: 'मातृभ्याम्',     acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'matr-f-ida-du-3', romaji: 'mātṛbhyām',      devanagari: 'मातृभ्याम्',     acceptableCells: ['cell-ins-du', 'cell-dat-du', 'cell-abl-du'] },
+    { id: 'matr-f-gl-du-1',  romaji: 'mātroḥ',         devanagari: 'मात्रोः',       acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
+    { id: 'matr-f-gl-du-2',  romaji: 'mātroḥ',         devanagari: 'मात्रोः',       acceptableCells: ['cell-gen-du', 'cell-loc-du'] },
+
+    // 複数 (Plural)
+    { id: 'matr-f-nav-pl-1', romaji: 'mātaraḥ',        devanagari: 'मातरः',       acceptableCells: ['cell-nom-pl', 'cell-voc-pl'] },
+    { id: 'matr-f-nav-pl-2', romaji: 'mātaraḥ',        devanagari: 'मातरः',       acceptableCells: ['cell-nom-pl', 'cell-voc-pl'] },
+    { id: 'matr-f-acc-pl',  romaji: 'mātṝḥ',          devanagari: 'मातॄः',        acceptableCells: ['cell-acc-pl'] },
+    { id: 'matr-f-ins-pl',  romaji: 'mātṛbhiḥ',       devanagari: 'मातृभिः',       acceptableCells: ['cell-ins-pl'] },
+    { id: 'matr-f-da-pl-1', romaji: 'mātṛbhyaḥ',      devanagari: 'मातृभ्यः',      acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'matr-f-da-pl-2', romaji: 'mātṛbhyaḥ',      devanagari: 'मातृभ्यः',      acceptableCells: ['cell-dat-pl', 'cell-abl-pl'] },
+    { id: 'matr-f-gen-pl',  romaji: 'mātṝṇām',        devanagari: 'मातॄणाम्',      acceptableCells: ['cell-gen-pl'] },
+    { id: 'matr-f-loc-pl',  romaji: 'mātṛṣu',         devanagari: 'मातृषु',        acceptableCells: ['cell-loc-pl'] }
+];
+
+// console.log("matr-f-data.js loaded");
+// console.log(matrFCardsData.length);
